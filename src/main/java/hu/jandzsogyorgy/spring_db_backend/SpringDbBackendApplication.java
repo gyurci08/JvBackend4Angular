7e5159240bf7e54f69086a0b3858718810032c70
gyoergy.jandzso@t-systems.com <gyoergy.jandzso@t-systems.com>
2024-12-02 16:39:56 +0100

    init

---
+++ src/main/java/hu/jandzsogyorgy/spring_db_backend/SpringDbBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.jandzsogyorgy.spring_db_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/jandzsogyorgy/spring_db_backend/SpringDbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.jandzsogyorgy.spring_db_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
